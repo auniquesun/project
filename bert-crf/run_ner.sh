a9545b044f79feb798cacafe5e79a05166f64290
Sun Hongyu <Mr.sunhy@outlook.com>
2019-11-13 00:14:58 +0800

    bilstm-crf

---
+++ bert-crf/run_ner.sh
@@ -0,0 +1,29 @@
+  #!/usr/bin/env bash
+
+  python BERT_NER_new.py\
+    --task_name="NER"  \
+    --do_lower_case=False \
+    --crf=False \
+    --do_train=True   \
+    --do_eval=True   \
+    --do_predict=True \
+    --do_gt=False \
+    --data_dir=data_new   \
+    --vocab_file=bert/cased_L-12_H-768_A-12/vocab.txt  \
+    --bert_config_file=bert/cased_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=bert/cased_L-12_H-768_A-12/bert_model.ckpt   \
+    --max_seq_length=22   \
+    --train_batch_size=64   \
+    --learning_rate=2e-5   \
+    --num_train_epochs=4.0   \
+    --output_dir=./output/result_dir
+
+# perl 是一个命令，具体文档可以自行查阅，conlleval.pl是一个脚本，用来评估序列标注模型的预测效果
+# 为啥不用这个默认的评估脚本的，因为我的数据集，和 conll 还是不太一样的，直接用会有问题，precision recall f1都没打出来
+# perl conlleval.pl -d '\t' < ./output/result_dir/label_test.txt
+
+# 这是关于 label_test.txt 文件格式的一些说明，来自官方文档 https://www.clips.uantwerpen.be/conll2003/ner/
+# The example deals with text chunking, a task which uses the same output format as this named entity task.
+#  The program requires the output of the NER system for each word to be appended to the corresponding line 
+#  in the test file, with a single space between the line and the output tag. Make sure you keep the empty 
+#  lines in the test file otherwise the software may mistakingly regard separate entities as one big entity.
---
+++ bilstm-crf/evaluate.py
@@ -0,0 +1,164 @@
+from model.data_utils import CoNLLDataset
+from model.ner_model import NERModel
+from model.config import Config
+
+from os import listdir
+from os.path import join, isfile
+
+
+def align_data(data):
+    """Given dict with lists, creates aligned strings
+
+    Adapted from Assignment 3 of CS224N
+
+    Args:
+        data: (dict) data["x"] = ["I", "love", "you"]
+              (dict) data["y"] = ["O", "O", "O"]
+
+    Returns:
+        data_aligned: (dict) data_align["x"] = "I love you"
+                           data_align["y"] = "O O    O  "
+
+    """
+    spacings = [max([len(seq[i]) for seq in data.values()])
+                for i in range(len(data[list(data.keys())[0]]))]
+    data_aligned = dict()
+
+    # for each entry, create aligned string
+    for key, seq in data.items():
+        str_aligned = ""
+        for token, spacing in zip(seq, spacings):
+            str_aligned += token + " " * (spacing - len(token) + 1)
+
+        data_aligned[key] = str_aligned
+
+    return data_aligned
+
+
+
+def interactive_shell(model):
+    """Creates interactive shell to play with model
+
+    Args:
+        model: instance of NERModel
+
+    """
+    way = input("choose the way of evaluation, please input number \n"
+    "                ----interactive(0)/batch(1): ")
+    if way == '0':
+        model.logger.info("""
+    This is an interactive mode.
+    To exit, enter 'exit'.
+    You can enter a sentence like
+    input> Deep Residual Learning for Image Recognition """)
+
+        while True:
+            try:
+                # for python 2
+                sentence = raw_input("input> ")
+            except NameError:
+                # for python 3
+                sentence = input("input> ")
+
+            words_raw = sentence.strip().split(" ")
+
+            if words_raw == ["exit"]:
+                break
+
+            preds = model.predict(words_raw)
+            to_print = align_data({"input": words_raw, "output": preds})
+
+            for key, seq in to_print.items():
+                model.logger.info(seq)
+    elif way == '1':
+        model.logger.info("""
+                >>> Results of prediction of the BiLSTM + CRF model
+                >>> will be saved in other/BiLSTM+CRF.results\n""")
+        # origin solution
+        # gt_name = input('please enter groudtruth number: ')
+        # with open('../other/groundtruth/' + gt_name + '.txt') as fin, \
+                # open('../other/BiLSTM+CRF.results', 'a') as fout:
+        gt_path = '../other/groundtruth/'
+        for f in listdir(gt_path):
+            if isfile(join(gt_path, f)) is True:
+                print('*************', f, '*************')
+                with open(join(gt_path, f)) as fin, \
+                    open(join('../other/', 'BiLSTM+CRF.results'), 'a') as fout:
+                    lines = fin.readlines()
+                    if len(lines) % 3 != 0:
+                        raise Exception('>>> File', f, 'format ERROR\n')
+
+                    words, labels, preds = None, None, None
+                    true, false = 0, 0
+                    tp, fp, fn, tn = 0, 0, 0, 0
+                    for idx, line in enumerate(lines):  # idx 从0开始                        
+                        if idx % 3 == 1:
+                            fout.write(line)
+                            words = line.strip().split(' ')
+                        elif idx % 3 == 2:
+                            preds = model.predict(words)
+                            fout.write(' '.join(preds) + '\n')
+                            labels = line.strip().split(' ')
+                            if len(labels) != len(preds):
+                                print('>>>', str(idx), 'ERROR: #labels != #preds', '<<<')
+                                print('>>>', str(idx), len(labels), len(preds), '<<<')
+                                print('labels:', labels)
+                                print('preds:', preds)
+                            
+                            for (label, pred) in zip(labels, preds):
+                                if label == pred:
+                                    true += 1
+                                    if label == 'P' or label == 'M':
+                                        tp += 1
+                                    else:
+                                        tn += 1
+                                else:
+                                    false += 1
+                                    if (label == 'P' or label == 'M') and pred == 'O':
+                                        fn += 1
+                                    elif (pred == 'P' or pred == 'M') and label == 'O':
+                                        fp += 1
+                        elif idx % 3 == 0:
+                            fout.write(line)
+                    if true+false:
+                        # print('#true:', true, ', #false:', false)
+                        print("loss:", false/(true+false))
+                        print("acc:", true/(true+false))
+                        precision, recall = tp/(tp+fp), tp/(tp+fn)
+                        print('precision:', precision)
+                        print('recall:', recall)
+                        print('f1 score:', 2*precision*recall/(precision+recall))
+                    else:
+                        print('both true and false are 0')
+                print()
+    else:
+        raise Exception('>>>Value of way matches nothing, please check your input')
+
+
+def main():
+    # create instance of config
+    config = Config()
+
+    # build model
+    model = NERModel(config)
+    model.build()
+    model.restore_session(config.dir_model)
+
+    option = input('>>>>> input the number of your option --\n'
+                    'evaluate_on_testset(0)/interactive_shell(1): '
+                )
+    if option == '0':
+        # 直接进去predict就行，没必要在test上evaluate，当然是可以的
+        # create dataset
+        test = CoNLLDataset(config.filename_test, config.processing_word,
+                            config.processing_tag, config.max_iter)
+        # evaluate and interact
+        model.evaluate(test)
+    elif option == '1':
+        interactive_shell(model)
+    else:
+        raise Exception('--> The number should be 0 or 1. -->')
+
+
+if __name__ == "__main__":
+    main()
