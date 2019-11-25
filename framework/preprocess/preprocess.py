2bb6e4e67c9ce408eda7e026bc07a05a87de407d
Sun Hongyu <Mr.sunhy@outlook.com>
2019-11-25 23:43:47 +0800

    update preprocess

--- framework/preprocess/preprocess.py
+++ framework/preprocess/preprocess.py
@@ -1,4 +1,9 @@
-# -*- coding: utf-8 -*-
+#! usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+# Copyright 2019 *** (for blind review)
+# Time:2019/11/14
+"""
 
 class Preprocess:
     def __init__(self):
