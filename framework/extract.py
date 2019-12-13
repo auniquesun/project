ecb8be3ad34f1067f88fcc0cd9685188399a17a5
Sun Hongyu <Mr.sunhy@outlook.com>
2019-12-13 11:21:54 +0800

    tranlate chinese into english

--- framework/extract.py
+++ framework/extract.py
@@ -9,14 +9,14 @@ Date: 2019/11/14
 
 def pattern_phrase_extraction():
     print('>> starting building standard pattern')
-    
+
     # input parameter: the set of known pattern words, KPW
-    # note: after bootstrapping iteration, the KPW has been merged with NPW so the KPW contains latest modification.    
+    # note: after bootstrapping iteration, the KPW has been merged with NPW so the KPW contains latest modification.
     patterns = []
     with open('known_pattern_words.txt') as fin:
         lines = fin.readlines()
         for line in lines:
-            if len(line.strip()) > 0 and '#' not in line:   # 去掉空行，注释行
+            if len(line.strip()) > 0 and '#' not in line:   # skip blank line and commented line
                 patterns.append(line.strip())
 
     pos_examples, neg_examples = [], []
@@ -24,7 +24,7 @@ def pattern_phrase_extraction():
     file_title = input('fin > enter title file name: ')
     # output: patterns in titles containing label id information
     file_patt = input('fout < enter pattern file name: ')
-    file_title_real = input('fout < enter real title file name: ')    
+    file_title_real = input('fout < enter real title file name: ')
     with open('title/' + file_title) as fin, open('pattern/' + file_patt, 'w') as fout, \
             open('title/' + file_title_real, 'w') as fout_real:
         lines = fin.readlines()
@@ -34,27 +34,27 @@ def pattern_phrase_extraction():
             if len(line.strip()) != 0:
                 tokens = line.split(' ')
                 tokens = [token for token in tokens if len(token) > 0]
-                # 删除 'Shift-Net: Image Inpainting via Deep Feature Rearrangement' 第一个词
-                # 标题中这种情况很多，对chunk影响比较大
+                # for this case: 'Shift-Net: Image Inpainting via Deep Feature Rearrangement', remove its first word
+                # because these cases have great influcen on chunking
                 if len(tokens):
                     print(index+1, '-->', tokens[0], '-->')
                 if len(tokens) > 0 and tokens[0][-1] == ':':
                     del tokens[0]
-                # token 可能是大写/小写，统一转换成小写
+                # convert token to the form of lower case
                 tokens_lower = [token.lower() for token in tokens]
-                # 用来标记不包含 pattern 的title
+                # count to record the title which didn't contain any pattern
                 count = 0
                 written_pattern = False
                 written_none = False
                 for pattern in patterns:
                     flag = False
-                    # pattern 都是小写
+                    # pattern is lower case
                     if pattern == 'based on':
                         if 'based' in tokens_lower and 'on' in tokens_lower:
                             if tokens_lower.index('on') - tokens_lower.index('based') == 1:
                                 flag = True
                     if pattern in tokens_lower or flag:
-                        #保证不会出现某个词包含pattern，然后把这个词切分的情况
+                        # make sure that a single word containing specific patterns won't be split
                         if flag == False:
                             idx = tokens_lower.index(pattern)
                             former = tokens[:idx]
@@ -71,20 +71,27 @@ def pattern_phrase_extraction():
                                 pos_examples.append(str(index+1) + ', P: ' + pattern + ', ' + former[0] + ', ' + latter[0] + '\n')
                                 fout_real.write(line)
                                 self.line_index += 1
-                                written_pattern = True  # 如果写入一个 pattern 对应的title，把count记作-2，用来break这个循环                                
+                                # fout wrote a title containing pattern, set 'written_pattern' true for breaking loop
+                                written_pattern = True
                         else:
                             """
                             句子中存在pattern word，但是len(former)，len(latter)中至少一个为0
-                            例，Using GLRPPR (and other P2Rx Centers) to Identify 
+                            例，Using GLRPPR (and other P2Rx Centers) to Identify
                             Hazardous Substance Reduction Resources
                             从这个例子想到另一个pattern，using ... to ...
-                            """                       
+                            """
                             written_none = True
-                    if pattern not in tokens_lower and flag == False: # pattern word不在句子里头
+                    if pattern not in tokens_lower and flag == False: # pattern word didn't appear in the sentence
                         count += 1
                     if written_pattern:
                         break
-                if count == len(patterns) or (not written_pattern and written_none):  # 没有一个pattern word在句子里头，或者句子里有pattern word，但是chunk之后的former/latter有一个为空
+                """
+                case one: the sentence didn't contain any pattern word
+                case two: there is a pattern word in the sentence but, len(former) or len(latter) equals 0 after chunking
+                """
+                # case one: the sentence didn't contain any pattern word
+                # case two: there is a pattern word in the sentence but, len(former) or len(latter) equals 0 after chunking
+                if count == len(patterns) or (not written_pattern and written_none):
                     if len(neg_examples) < 500000:
                         neg_examples.append(str(index+1) + ', P: None' + '\n')
                         fout_real.write(line)
@@ -98,4 +105,4 @@ def pattern_phrase_extraction():
 
 if __name__ == '__main__':
     pattern_phrase_extraction()
-    
+
